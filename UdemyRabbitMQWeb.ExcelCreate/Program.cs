a498fd06f517d2757a332b70d589a52fe34a9e5f
Aygün Bayır <turkoaygun@gmail.com>
2025-01-15 12:55:15 +0300

    Add Identity

--- UdemyRabbitMQWeb.ExcelCreate/Program.cs
+++ UdemyRabbitMQWeb.ExcelCreate/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyRabbitMQWeb.ExcelCreate.Models;
 
 namespace UdemyRabbitMQWeb.ExcelCreate
 {
@@ -13,7 +18,37 @@ namespace UdemyRabbitMQWeb.ExcelCreate
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Uygulama ayaða kalkarken migrationlarý yapar.
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                appDbContext.Database.Migrate();
+
+                if(!appDbContext.Users.Any())
+                {
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme",
+                        Email = "deneme@outlook.com"
+                    }, "Password12*").Wait();
+
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme2",
+                        Email = "deneme2@outlook.com"
+                    }, "Password12*").Wait();
+                }
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- UdemyRabbitMQWeb.ExcelCreate/Startup.cs
+++ UdemyRabbitMQWeb.ExcelCreate/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UdemyRabbitMQWeb.ExcelCreate.Models;
 
 namespace UdemyRabbitMQWeb.ExcelCreate
 {
@@ -23,6 +26,14 @@ namespace UdemyRabbitMQWeb.ExcelCreate
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+            });
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
             services.AddControllersWithViews();
         }
 
@@ -43,7 +54,7 @@ namespace UdemyRabbitMQWeb.ExcelCreate
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
